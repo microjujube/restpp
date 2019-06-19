f494a16e6220ed96d5b3260cf918f2fda7b46eab
linwei <zlinwei@zlinwei.com>
2019-06-19 11:01:55 +0800

    handle session and call handle requests;

--- lib/Engine.cpp
+++ lib/Engine.cpp
@@ -14,6 +14,7 @@
 #include <string>
 #include <thread>
 
+#include "resetpp/Logger.hpp"
 #include "resetpp/Engine.hpp"
 
 using namespace resetpp;
@@ -37,6 +38,8 @@ void EngineSync::Run() {
     auto const port = 8080;
     std::string const doc_root = "./static";
 
+    LOG(INFO) << "Server on: " << address << ":" << port;
+
     // The io_context is required for all I/O
     boost::asio::io_context ioc{1};
 
@@ -57,6 +60,75 @@ void EngineSync::Run() {
     }
 }
 
-void EngineSync::do_session(boost::asio::ip::tcp::socket &socket, std::string const &doc_root) {
+// This is the C++11 equivalent of a generic lambda.
+// The function object is used to send an HTTP message.
+template<class Stream>
+struct send_lambda {
+    Stream &stream_;
+    bool &close_;
+    boost::system::error_code &ec_;
 
+    explicit
+    send_lambda(
+            Stream &stream,
+            bool &close,
+            boost::system::error_code &ec)
+            : stream_(stream), close_(close), ec_(ec) {
+    }
+
+    template<bool isRequest, class Body, class Fields>
+    void
+    operator()(boost::beast::http::message<isRequest, Body, Fields> &&msg) const {
+        // Determine if we should close the connection after
+        close_ = msg.need_eof();
+
+        // We need the serializer here because the serializer requires
+        // a non-const file_body, and the message oriented version of
+        // http::write only works with const messages.
+        boost::beast::http::serializer<isRequest, Body, Fields> sr{msg};
+        boost::beast::http::write(stream_, sr, ec_);
+    }
+};
+
+
+// Report a failure
+void
+fail(boost::system::error_code ec, char const *what) {
+    std::cerr << what << ": " << ec.message() << "\n";
+}
+
+void EngineSync::do_session(boost::asio::ip::tcp::socket &socket, std::string const &doc_root) {
+    bool close = false;
+    boost::system::error_code ec;
+
+    // This buffer is required to persist across reads
+    boost::beast::flat_buffer buffer;
+
+    // This lambda is used to send messages
+    send_lambda<boost::asio::ip::tcp::socket> lambda{socket, close, ec};
+
+    for (;;) {
+        // Read a request
+        boost::beast::http::request<boost::beast::http::string_body> req;
+        boost::beast::http::read(socket, buffer, req, ec);
+        if (ec == boost::beast::http::error::end_of_stream)
+            break;
+        if (ec)
+            return fail(ec, "read");
+
+        // Send the response
+//        handle_request(doc_root, std::move(req), lambda);
+        if (ec)
+            return fail(ec, "write");
+        if (close) {
+            // This means we should close the connection, usually because
+            // the response indicated the "Connection: close" semantic.
+            break;
+        }
+    }
+
+    // Send a TCP shutdown
+    socket.shutdown(boost::asio::ip::tcp::socket::shutdown_send, ec);
+
+    // At this point the connection is closed gracefully
 }
